be0e2401684343784721d6479471600e7c7421de
tecno port <60147934+tecnoport@users.noreply.github.com>
2020-01-30 19:15:05 -0800

    done

---
+++ ForgetPassword/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace ForgetPassword
+{
+    partial class frmForgetPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmForgetPassword));
+            this.imgNextStep1 = new System.Windows.Forms.Button();
+            this.imgNextStep2 = new System.Windows.Forms.Button();
+            this.lblNextStep1 = new System.Windows.Forms.Label();
+            this.lblNextStep2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnForgetPasswordSendMail = new System.Windows.Forms.Button();
+            this.btnForgetPassordSendAgain = new System.Windows.Forms.Button();
+            this.btnForgetPassordNextStep = new System.Windows.Forms.Button();
+            this.btnForgetPasswordChange = new System.Windows.Forms.Button();
+            this.txtForgrtPassswordCode = new System.Windows.Forms.TextBox();
+            this.txtForgetPasswordConformPassword = new System.Windows.Forms.TextBox();
+            this.txtForgetPasswordNewPassword = new System.Windows.Forms.TextBox();
+            this.txtForgetPasswordEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // imgNextStep1
+            // 
+            this.imgNextStep1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.imgNextStep1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.imgNextStep1.ForeColor = System.Drawing.Color.White;
+            this.imgNextStep1.Image = ((System.Drawing.Image)(resources.GetObject("imgNextStep1.Image")));
+            this.imgNextStep1.Location = new System.Drawing.Point(93, 122);
+            this.imgNextStep1.Name = "imgNextStep1";
+            this.imgNextStep1.Size = new System.Drawing.Size(38, 33);
+            this.imgNextStep1.TabIndex = 23;
+            this.imgNextStep1.UseVisualStyleBackColor = true;
+            this.imgNextStep1.Visible = false;
+            // 
+            // imgNextStep2
+            // 
+            this.imgNextStep2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.imgNextStep2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.imgNextStep2.ForeColor = System.Drawing.Color.White;
+            this.imgNextStep2.Image = ((System.Drawing.Image)(resources.GetObject("imgNextStep2.Image")));
+            this.imgNextStep2.Location = new System.Drawing.Point(93, 297);
+            this.imgNextStep2.Name = "imgNextStep2";
+            this.imgNextStep2.Size = new System.Drawing.Size(38, 33);
+            this.imgNextStep2.TabIndex = 24;
+            this.imgNextStep2.UseVisualStyleBackColor = true;
+            this.imgNextStep2.Visible = false;
+            // 
+            // lblNextStep1
+            // 
+            this.lblNextStep1.AutoSize = true;
+            this.lblNextStep1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNextStep1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblNextStep1.Location = new System.Drawing.Point(17, 130);
+            this.lblNextStep1.Name = "lblNextStep1";
+            this.lblNextStep1.Size = new System.Drawing.Size(81, 19);
+            this.lblNextStep1.TabIndex = 21;
+            this.lblNextStep1.Text = "Next Step";
+            this.lblNextStep1.Visible = false;
+            // 
+            // lblNextStep2
+            // 
+            this.lblNextStep2.AutoSize = true;
+            this.lblNextStep2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNextStep2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblNextStep2.Location = new System.Drawing.Point(17, 305);
+            this.lblNextStep2.Name = "lblNextStep2";
+            this.lblNextStep2.Size = new System.Drawing.Size(81, 19);
+            this.lblNextStep2.TabIndex = 22;
+            this.lblNextStep2.Text = "Next Step";
+            this.lblNextStep2.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Blue;
+            this.label7.Location = new System.Drawing.Point(18, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(481, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "_______________________________________________________________________________";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(18, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(481, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "_______________________________________________________________________________";
+            // 
+            // btnForgetPasswordSendMail
+            // 
+            this.btnForgetPasswordSendMail.BackColor = System.Drawing.Color.Red;
+            this.btnForgetPasswordSendMail.FlatAppearance.BorderSize = 0;
+            this.btnForgetPasswordSendMail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForgetPasswordSendMail.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForgetPasswordSendMail.ForeColor = System.Drawing.Color.White;
+            this.btnForgetPasswordSendMail.Location = new System.Drawing.Point(172, 78);
+            this.btnForgetPasswordSendMail.Name = "btnForgetPasswordSendMail";
+            this.btnForgetPasswordSendMail.Size = new System.Drawing.Size(125, 37);
+            this.btnForgetPasswordSendMail.TabIndex = 16;
+            this.btnForgetPasswordSendMail.Text = "Send Mail";
+            this.btnForgetPasswordSendMail.UseVisualStyleBackColor = false;
+            this.btnForgetPasswordSendMail.Click += new System.EventHandler(this.btnForgetPasswordSendMail_Click);
+            // 
+            // btnForgetPassordSendAgain
+            // 
+            this.btnForgetPassordSendAgain.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnForgetPassordSendAgain.Enabled = false;
+            this.btnForgetPassordSendAgain.FlatAppearance.BorderSize = 0;
+            this.btnForgetPassordSendAgain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForgetPassordSendAgain.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForgetPassordSendAgain.ForeColor = System.Drawing.Color.White;
+            this.btnForgetPassordSendAgain.Location = new System.Drawing.Point(303, 232);
+            this.btnForgetPassordSendAgain.Name = "btnForgetPassordSendAgain";
+            this.btnForgetPassordSendAgain.Size = new System.Drawing.Size(125, 37);
+            this.btnForgetPassordSendAgain.TabIndex = 17;
+            this.btnForgetPassordSendAgain.Text = "Send Again";
+            this.btnForgetPassordSendAgain.UseVisualStyleBackColor = false;
+            this.btnForgetPassordSendAgain.Click += new System.EventHandler(this.btnForgetPassordSendAgain_Click_1);
+            // 
+            // btnForgetPassordNextStep
+            // 
+            this.btnForgetPassordNextStep.BackColor = System.Drawing.Color.Blue;
+            this.btnForgetPassordNextStep.Enabled = false;
+            this.btnForgetPassordNextStep.FlatAppearance.BorderSize = 0;
+            this.btnForgetPassordNextStep.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForgetPassordNextStep.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForgetPassordNextStep.ForeColor = System.Drawing.Color.White;
+            this.btnForgetPassordNextStep.Location = new System.Drawing.Point(105, 232);
+            this.btnForgetPassordNextStep.Name = "btnForgetPassordNextStep";
+            this.btnForgetPassordNextStep.Size = new System.Drawing.Size(125, 37);
+            this.btnForgetPassordNextStep.TabIndex = 18;
+            this.btnForgetPassordNextStep.Text = "Next Step";
+            this.btnForgetPassordNextStep.UseVisualStyleBackColor = false;
+            this.btnForgetPassordNextStep.Click += new System.EventHandler(this.btnForgetPassordNextStep_Click_1);
+            // 
+            // btnForgetPasswordChange
+            // 
+            this.btnForgetPasswordChange.BackColor = System.Drawing.Color.Blue;
+            this.btnForgetPasswordChange.Enabled = false;
+            this.btnForgetPasswordChange.FlatAppearance.BorderSize = 0;
+            this.btnForgetPasswordChange.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForgetPasswordChange.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForgetPasswordChange.ForeColor = System.Drawing.Color.White;
+            this.btnForgetPasswordChange.Location = new System.Drawing.Point(172, 444);
+            this.btnForgetPasswordChange.Name = "btnForgetPasswordChange";
+            this.btnForgetPasswordChange.Size = new System.Drawing.Size(125, 37);
+            this.btnForgetPasswordChange.TabIndex = 15;
+            this.btnForgetPasswordChange.Text = "Change";
+            this.btnForgetPasswordChange.UseVisualStyleBackColor = false;
+            this.btnForgetPasswordChange.Click += new System.EventHandler(this.btnForgetPasswordChange_Click_1);
+            // 
+            // txtForgrtPassswordCode
+            // 
+            this.txtForgrtPassswordCode.Enabled = false;
+            this.txtForgrtPassswordCode.Location = new System.Drawing.Point(200, 188);
+            this.txtForgrtPassswordCode.Name = "txtForgrtPassswordCode";
+            this.txtForgrtPassswordCode.Size = new System.Drawing.Size(244, 20);
+            this.txtForgrtPassswordCode.TabIndex = 12;
+            // 
+            // txtForgetPasswordConformPassword
+            // 
+            this.txtForgetPasswordConformPassword.Enabled = false;
+            this.txtForgetPasswordConformPassword.Location = new System.Drawing.Point(200, 400);
+            this.txtForgetPasswordConformPassword.Name = "txtForgetPasswordConformPassword";
+            this.txtForgetPasswordConformPassword.Size = new System.Drawing.Size(244, 20);
+            this.txtForgetPasswordConformPassword.TabIndex = 11;
+            // 
+            // txtForgetPasswordNewPassword
+            // 
+            this.txtForgetPasswordNewPassword.Enabled = false;
+            this.txtForgetPasswordNewPassword.Location = new System.Drawing.Point(200, 358);
+            this.txtForgetPasswordNewPassword.Name = "txtForgetPasswordNewPassword";
+            this.txtForgetPasswordNewPassword.PasswordChar = '*';
+            this.txtForgetPasswordNewPassword.Size = new System.Drawing.Size(244, 20);
+            this.txtForgetPasswordNewPassword.TabIndex = 14;
+            // 
+            // txtForgetPasswordEmail
+            // 
+            this.txtForgetPasswordEmail.Location = new System.Drawing.Point(200, 28);
+            this.txtForgetPasswordEmail.Name = "txtForgetPasswordEmail";
+            this.txtForgetPasswordEmail.Size = new System.Drawing.Size(244, 20);
+            this.txtForgetPasswordEmail.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(38, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 21);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Four Digit Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(38, 397);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Conform Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(38, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "New Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(137, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(223, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "This Email is not exist in data base";
+            this.label4.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(38, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Enter your Email";
+            // 
+            // frmForgetPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 500);
+            this.Controls.Add(this.imgNextStep1);
+            this.Controls.Add(this.imgNextStep2);
+            this.Controls.Add(this.lblNextStep1);
+            this.Controls.Add(this.lblNextStep2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnForgetPasswordSendMail);
+            this.Controls.Add(this.btnForgetPassordSendAgain);
+            this.Controls.Add(this.btnForgetPassordNextStep);
+            this.Controls.Add(this.btnForgetPasswordChange);
+            this.Controls.Add(this.txtForgrtPassswordCode);
+            this.Controls.Add(this.txtForgetPasswordConformPassword);
+            this.Controls.Add(this.txtForgetPasswordNewPassword);
+            this.Controls.Add(this.txtForgetPasswordEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "frmForgetPassword";
+            this.Text = "Forget Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button imgNextStep1;
+        private System.Windows.Forms.Button imgNextStep2;
+        private System.Windows.Forms.Label lblNextStep1;
+        private System.Windows.Forms.Label lblNextStep2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnForgetPasswordSendMail;
+        private System.Windows.Forms.Button btnForgetPassordSendAgain;
+        private System.Windows.Forms.Button btnForgetPassordNextStep;
+        private System.Windows.Forms.Button btnForgetPasswordChange;
+        private System.Windows.Forms.TextBox txtForgrtPassswordCode;
+        private System.Windows.Forms.TextBox txtForgetPasswordConformPassword;
+        private System.Windows.Forms.TextBox txtForgetPasswordNewPassword;
+        private System.Windows.Forms.TextBox txtForgetPasswordEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ ForgetPassword/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Data.OleDb;
+
+namespace ForgetPassword
+{
+    public partial class frmForgetPassword : Form
+    {
+        Random rnd = new Random();
+      public int code ;
+        
+        //Connect database, copy and change the location of database blow
+      OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\ForgetPassword-master\ForgetPassword\ForgetPassword.mdb");
+
+        public frmForgetPassword()
+        {
+            InitializeComponent();
+        }
+
+
+        private void mail()
+        {
+
+            code = rnd.Next(123123, 999999);
+            const string p = "sent password";
+
+
+            MailMessage message = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+
+            message.From = new MailAddress("Sent email");
+
+            //Enter your email blow and also change in database too
+
+            message.To.Add(new MailAddress("recive email"));
+            message.Subject = "change password";
+            message.Body = "Write this given code on text box\n" + code + "\nThank you!";
+
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("sent email", p);
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Send(message);
+            MessageBox.Show("Email has been sent");
+
+
+        }
+
+
+        private void btnForgetPasswordChange_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnForgetPassordNextStep_Click_1(object sender, EventArgs e)
+        {
+            if (code.ToString() == txtForgrtPassswordCode.Text)
+            {
+                lblNextStep2.Visible = true;
+                imgNextStep2.Visible = true;
+
+                txtForgetPasswordConformPassword.Enabled = true;
+                txtForgetPasswordNewPassword.Enabled = true;
+                btnForgetPasswordChange.Enabled = true;
+                btnForgetPassordNextStep.Enabled = false;
+                btnForgetPassordSendAgain.Enabled = false;
+            }
+
+            else
+            {
+                MessageBox.Show("Code doesent Match");
+            }
+      
+        }
+
+        private void btnForgetPassordSendAgain_Click_1(object sender, EventArgs e)
+        {
+            mail();
+        }
+
+        private void btnForgetPasswordChange_Click_1(object sender, EventArgs e)
+        {
+            if (txtForgetPasswordNewPassword.Text == txtForgetPasswordConformPassword.Text)
+            {
+                string query = "update ownerInfo set [password]='" + txtForgetPasswordConformPassword.Text + "' where ID=1";
+                OleDbCommand cmd = new OleDbCommand(query, con);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+
+                MessageBox.Show("Pasword changes succussfully");
+                this.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("password dosent match");
+            }
+        }
+
+        private void btnForgetPasswordSendMail_Click(object sender, EventArgs e)
+        {
+
+            string query = "select Email from ownerInfo";
+            OleDbCommand cmd = new OleDbCommand(query, con);
+
+            DataTable dt = new DataTable();
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            con.Close();
+            string txtboxEmail = txtForgetPasswordEmail.Text;
+            foreach (DataRow dr in dt.Rows)
+            {
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    if (txtboxEmail == dr[dc].ToString())
+                    {
+                        lblNextStep1.Visible = true;
+                        imgNextStep1.Visible = true;
+
+                        txtForgrtPassswordCode.Enabled = true;
+                        btnForgetPassordNextStep.Enabled = true;
+                        btnForgetPassordSendAgain.Enabled = true;
+                        btnForgetPasswordSendMail.Enabled = false;
+
+                        mail();
+                    }
+                    else
+                    {
+                        MessageBox.Show("This email is not exisit in the given data");
+
+                    }
+
+
+                }
+            }
+            
+
+        }
+
+
+        }
+
+}
